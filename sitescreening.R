f71fb8d8f25c9da6f359407a2edf82db683ad3cd
hen-ald <henrik.alden@regiondalarna.se>
2023-09-21 15:16:00 +0200

    nerbantad karta

--- sitescreening.R
+++ sitescreening.R
@@ -31,9 +31,7 @@ con <- dbConnect(          # use in other settings
   dbname = "geodata",
   options="-c search_path=public")
 
-kommuner <- st_read(con, query = "SELECT * FROM karta.kommun_scb") 
-
-kommun <- kommuner %>% 
+kommun <- st_read(con, query = "SELECT * FROM karta.kommun_scb") %>% 
   filter(lanskod_tx == 20) %>%    ##ändra länskod_tx till din regionkod
   select("kom_kod" = "knkod",
          "kommun" = "knnamn",
@@ -87,58 +85,71 @@ industriomr <- st_read(industriomr_fil, crs = 3006)
 
 #Elinfrastruktur
 transformatoromr_fil <- "G:/skript/henrik/GitHub/Region-Atlas/fran_philip/transformatoromr.gpkg"
-transformatoromr <- st_read(transformatoromr_fil, crs = 3006)
+transformatoromr <- st_read(transformatoromr_fil, crs = 3006) %>% 
+  st_buffer(100)
 
 elledningar_fil <- "G:/Samhällsanalys/GIS/projekt/Invest in Dalarna/utdata/elledningar/ledningar_ellevio.gpkg" #G:\Samhällsanalys\GIS\projekt\Invest in Dalarna\utdata\elledningar
 elledningar <- st_read(elledningar_fil, crs = 3006)
 
+
 #3 speciella transformatorstationer
-repbacken_etc_fil <- "G:/Samhällsanalys/GIS/projekt/Invest in Dalarna/utdata/Repbäcken_etc.gpkg" 
-transformator_vip <- st_read(repbacken_etc_fil, crs = 3006) %>% 
+repbacken_etc_fil <- "G:/Samhällsanalys/GIS/projekt/Invest in Dalarna/utdata/Repbäcken_etc.gpkg"
+transformator_vip <- st_read(repbacken_etc_fil, crs = 3006) %>%
   st_buffer(1000)
 
 #flygplatser
 
 flygplats_fil <- "G:/Samhällsanalys/GIS/projekt/Invest in Dalarna/utdata/Flygplats.gpkg"
 
-flygplats_1 <- st_read(flygplats_fil, crs = 3006) %>% 
-  mutate(rep(c("Dalaflyget", "Dalaflyget", "Scandinavian Mountains AB"))) %>% 
-  mutate(rep(c("Dala Airport", "Mora/Siljan Flygplats", "Sälen/Trysil Airport"))) 
+flygplats_1 <- st_read(flygplats_fil, crs = 3006) %>%
+  mutate(rep(c("Dalaflyget", "Dalaflyget", "Scandinavian Mountains AB"))) %>%
+  mutate(rep(c("Dala Airport", "Mora/Siljan Flygplats", "Sälen/Trysil Airport")))
 
-glimpse(flygplats_1)
-flygplats <- flygplats_1 %>% 
+#glimpse(flygplats_1)
+flygplats <- flygplats_1 %>%
   rename(operator = `rep(c("Dalaflyget", "Dalaflyget", "Scandinavian Mountains AB"))`,
          namn = `rep(c("Dala Airport", "Mora/Siljan Flygplats", "Sälen/Trysil Airport"))`)
+
+# Stoplager
+# bostader
+bostad_buffert_fil <- "G:/Samhällsanalys/GIS/projekt/Invest in Dalarna/utdata/till_rmd/bostad_buff_simp.gpkg"
+bostader <- st_read(bostad_buffert_fil, crs = 3006)
+
+#Vatten
+vatten_dalarna_fil <- "G:/Samhällsanalys/GIS/projekt/Invest in Dalarna/utdata/till_rmd/vatten_buff_simp.gpkg"
+vatten <- st_read(vatten_dalarna_fil, crs = 3006)
+
+#Jordbruksmark
+jordbruk_fil <- "G:/Samhällsanalys/GIS/projekt/Invest in Dalarna/utdata/till_rmd/jord_buff_simp.gpkg"
+jordbruk <- st_read(jordbruk_fil, crs = 3006)
+
 #kartan
 
-mapview(vectoriserad)
+#mapview(vectoriserad)
 
 skane = colorRampPalette(c("lightyellow", 'yellow', "orange", "red", 'darkred'))
 
-mapview(vectoriserad, homebutton = FALSE, alpha.regions = 0.6, lwd = 0.0, col.regions = skane)
+#mapview(vectoriserad, homebutton = FALSE, alpha.regions = 0.6, lwd = 0.0, col.regions = skane)
 
 greys <- colorRampPalette(c("grey50", "grey87"))
 
-mapview(nvdb_dalarna, zcol = "vagnummer", color = greys, label = "vagnummer", lwd = 2, alpha = 0.5, legend = FALSE, homebutton = FALSE, hide = TRUE, layer.name = "Stora vagar")
+greens <- colorRampPalette(c("green1", "green3", ("green4")))
 
 
-#Kartan från RMD
-
-mapview(kommun, alpha = TRUE, legend = FALSE, alpha.regions = 0.0, label = "kommun", hide = TRUE)+
-  #mapview(jarnvagstation, col.regions = "white", color = "black", cex = 1, legend = FALSE, label = "objekttyp", homebutton = FALSE)+
+mapview(kommun, alpha = TRUE, legend = FALSE, alpha.regions = 0.0, label = "kommun", hide = TRUE, homebutton = FALSE)+
   mapview(jarnvag, color = "grey29", lwd = 2, legend = FALSE, label = "straknamn", homebutton = FALSE, hide = TRUE)+
-  mapview(vectoriserad, layer.name = "poangsattning", homebutton = FALSE, alpha.regions = 0.5, lwd = 0.0, col.regions = skane)+ # ändra färg
+  mapview(vectoriserad, layer.name = "Sitescreening", homebutton = FALSE, alpha.regions = 0.5, lwd = 0.0, col.regions = skane)+ # ändra färg
   mapview(utvalda_fastigheter, label = "fastighet", legend = FALSE, homebutton = FALSE, alpha.regions = 0.5, col.regions = c("purple"), hide = TRUE)+
   mapview(transformatoromr, label = "objekttyp", homebutton = FALSE, col.regions = c("green"), legend = FALSE, hide = TRUE)+#transformatoromr
-  mapview(industriomr, label = "objekttyp", homebutton = FALSE, alpha.regions = 0.5, col.regions = c("brown"), legend = FALSE, hide = TRUE)+#industriomr
+  mapview(industriomr, label = "objekttyp", homebutton = FALSE, alpha.regions = 0.5, col.regions = c("darkred"), legend = FALSE, hide = TRUE)+#industriomr
   mapview(naturvard, label = "objekttyp", homebutton = FALSE, alpha.regions = 0.5, col.regions = c("darkgreen"), legend = FALSE, hide = TRUE)+##naturvard
-  mapview(elledningar, color = "green", lwd = 2, legend = FALSE, label = "objekttyp", homebutton = FALSE, hide = TRUE)+
+  mapview(elledningar, color = greens, zcol = "objekttyp", lwd = 2, legend = FALSE, label = "objekttyp", homebutton = FALSE, hide = TRUE)+
   mapview(nvdb_dalarna, zcol = "vagnummer", color = greys, label = "vagnummer", lwd = 2, alpha = 0.5, legend = FALSE, homebutton = FALSE, hide = TRUE, layer.name = "vagar")+
-  mapview(flygplats, col.regions = "red", color = "white", cex = 6, legend = FALSE, label = "namn", homebutton = FALSE, hide = TRUE)+
-  mapview(transformator_vip, col.regions = "green", color = "white", cex = 6, legend = FALSE, label = "objekttyp", homebutton = FALSE, hide = TRUE)
-
-
-
+  mapview(flygplats, col.regions = "red", color = "white", cex = 6, legend = FALSE, label = "namn", homebutton = TRUE, hide = TRUE)+
+  mapview(transformator_vip, col.regions = "green", color = "white", cex = 6, legend = FALSE, label = "objekttyp", homebutton = TRUE, hide = TRUE)+
+  mapview(vatten, label = "NAMN1", col.regions = "dodgerblue4", alpha.regions = 0.9, homebutton = FALSE, hide = TRUE, legend = FALSE)+
+  mapview(jordbruk, label = "objekttyp", col.regions = "chocolate4", alpha.regions = 0.9, homebutton = FALSE, hide = TRUE, legend = FALSE)+
+  mapview(bostader, label = "objekttyp", col.regions = "black", alpha.regions = 0.9, homebutton = FALSE, hide = TRUE, legend = FALSE)
 
 
 
